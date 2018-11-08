7162a3e7ebdd04e480115a41be6e1a3f6e19f617
artemama <artsiom.ivahno@gmail.com>
2018-11-08 07:52:50 +0300

    user

---
+++ app/src/main/java/Model/User.java
@@ -0,0 +1,47 @@
+package Model;
+
+public class User {String uid, email, name, surname;
+
+
+
+    public User(String uid, String email, String name, String surname, Enum Gender) {
+        this.uid = uid;
+        this.email = email;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+
+}
